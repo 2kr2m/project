c911c134d50155bf68d913c45e8c29efdcf36ea1
2kr2m <ak1ram@outlook.com>
2022-12-01 23:46:05 +0100

    another commit

---
+++ src/proj1main/HelloWorld.java
@@ -0,0 +1,11 @@
+package proj1main;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello this is your code from DEVOPS PROJECT");
+
+	}
+
+}
